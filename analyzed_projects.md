02ed25b81e7e897a5be9d1abf9496e3810e1ce7c
Justin <jjpeterson90@hotmail.com>
2022-06-08 14:22:47 -0500

    complete

---
+++ analyzed_projects.md
@@ -0,0 +1,14 @@
+Week-01:
+    Day1:
+        algo-99-bottles:
+            O(N) - single recursive call equates to one loop
+        algo-deaf-grandma:
+            O(N) - One for loop is used to iterate over user-generated reply
+    Day2:
+        algo-factorial:
+            O(N) - single recursive call equates to one loop
+        algo-fibonacci:
+            O(N^2) - (before change) double recursive call
+            O(N) - (after change) implemented memoization
+        algo-linear-search:
+            O(N) - single for loops used
--- big_O_challenge.py
+++ big_O_challenge.py
@@ -1,1 +1,43 @@
-# Explain, optimize, and benchmark your python code here.
+def iterate_a_lot():
+  for i in range(1000):
+    for j in range(100):
+      j
+
+def iterate_a_little():
+  for i in range(10):
+    i 
+
+
+# Reporting
+import time
+import random
+import statistics
+
+def fibonacci(n = 25, fib_memo = {}):
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  if n not in fib_memo:
+    fib_memo[n] = fibonacci(n-1) + fibonacci(n-2)
+  return fib_memo[n]
+
+functions = iterate_a_lot, iterate_a_little
+
+# this is just a one line way to make this: {'iterate_a_lot': [], 'iterate_a_little': []}
+times = {f.__name__: [] for f in functions}
+
+for func in functions:
+  for i in range(500):  # adjust accordingly so whole thing takes a few sec
+    t0 = time.time()
+    func()
+    t1 = time.time()
+    times[func.__name__].append((t1 - t0) * 1000)
+
+
+
+for name, numbers in times.items():
+  print('FUNCTION:', name, 'Used', len(numbers), 'times')
+  print('\tMEDIAN', statistics.median(numbers))
+  print('\tMEAN  ', statistics.mean(numbers))
+  print('\tSTDEV ', statistics.stdev(numbers))
